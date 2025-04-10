9f3b73c93b0e396b012be952f22872353b2182ee
Minas-shannak <minas.shannak200@gmail.com>
2025-04-10 12:13:39 +0300

    create dashboard

--- Day 3 – Git, Asynchronous JavaScript & CSS Layouts/dashboard/script.js
+++ Day 3 – Git, Asynchronous JavaScript & CSS Layouts/dashboard/script.js
@@ -17,6 +17,49 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
+    // bult in code from https://www.w3schools.com 
+    const performanceLabels = ["Q1", "Q2", "Q3", "Q4", "Total"];
+    const performanceData = [3500, 4000, 2800, 6200, 8500];
+
+    const trafficLabels = ["Organic", "Referral", "Direct", "Email", "Social"];
+    const trafficData = [5000, 2000, 1500, 800, 1200];
+
+    const performanceANDtrafficColors = ["red", "green", "blue", "orange", "brown"];
+
+    new Chart("BarCharts", {
+    type: "bar",
+    data: {
+        labels: performanceLabels,
+        datasets: [{
+        backgroundColor: performanceANDtrafficColors,
+        data: performanceData
+        }]
+    },
+    options: {
+        legend: { display: false },
+        title: {
+        display: true,
+        }
+    }
+    });
+
+
+    new Chart("PieCharts", {
+    type: "pie",
+    data: {
+        labels: trafficLabels,
+        datasets: [{
+        backgroundColor: performanceANDtrafficColors,
+        data: trafficData
+        }]
+    },
+    options: {
+        title: {
+        display: true,
+        }
+    }
+    });
+
 });
 
 
