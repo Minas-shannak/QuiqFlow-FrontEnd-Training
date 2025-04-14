de91f23e8b57cf7a8ec95a1719a851db604af027
Minas-shannak <minas.shannak200@gmail.com>
2025-04-14 11:25:54 +0300

    add Daly-Problem-solve folder

---
+++ Daly-Problem-solve/closures.js
@@ -0,0 +1,37 @@
+// Problem 1:
+/*
+function pow(num1){
+        return function (num2){
+            return Math.pow(num2 , num1);
+        }
+}
+var square = pow(2);
+var cube = pow(3);
+
+console.log(square(3));
+console.log(cube(3));
+*/
+// Problem 2:
+function pingPongTracker(){
+    let time=0;
+    return{
+        timeSpentPlaying :function(){
+            return time;
+        } ,
+        playOneGame : function(){
+            time += 15;
+            return "Game played"
+        },
+        myLevel : function(){
+            if(time < 30) return"I need to improve my game"
+                else if(time < 100)return"You need to improve your game"
+                    else return"Wow, I have wasted a lot of time"
+        },
+    }
+}
+
+var myGame = pingPongTracker();
+console.log(myGame.playOneGame());
+console.log(myGame.playOneGame());
+console.log(myGame.timeSpentPlaying());
+console.log(myGame.myLevel());
