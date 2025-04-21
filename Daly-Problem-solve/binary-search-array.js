93b2d2569620f00d7014e97f53c22f13aa1102a1
Minas-shannak <minas.shannak200@gmail.com>
2025-04-21 12:26:51 +0300

    Daly-Problem-solve(add note)

--- Daly-Problem-solve/binary-search-array.js
+++ Daly-Problem-solve/binary-search-array.js
@@ -23,3 +23,28 @@ const binarySearch = (arr, target) => {
     return "not found";
 }
 console.log(binarySearch(arr,11))
+
+/*
+After re-reading the code for the last time, I noticed that I repeated this line
+mid = Math.floor((low + high) / 2);
+Instead, it is preferable to define it within the loop
+*/
+
+/*
+const binarySearch = (arr, target) => {
+    let low = 0;
+    let high = arr.length - 1;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            low = mid +1 ;
+        } else{
+            high = mid -1 ;
+        }
+    }
+    return "not found";
+}
+*/
